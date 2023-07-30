d5077960f40d288a0f0a2436da8182097d5ac15a
Marcos J. (MJ2RS) <marcos.mj2rs@gmail.com>
2023-07-30 18:11:09 -0300

    Add project files.

---
+++ Kafka.Consumer.Handler/Handlers/MessageHandler.cs
@@ -0,0 +1,54 @@
+﻿using Confluent.Kafka;
+
+namespace Kafka.Consumer.Handler.Handlers
+{
+	public class MessageHandler : IHostedService
+	{
+		private readonly ILogger _logger;
+		public MessageHandler(ILogger<MessageHandler> logger)
+		{
+			_logger = logger;
+		}
+
+		public Task StartAsync(CancellationToken cancellationToken)
+		{
+			/*
+			 * Rodar o comando abaixo em um terminal docker para saner o ip do container
+			 * docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' kafka
+			 */
+
+			var conf = new ConsumerConfig
+			{
+				GroupId = "test-consumer-group",
+				BootstrapServers = "172.19.0.5:9092",
+				AutoOffsetReset = AutoOffsetReset.Earliest
+			};
+
+			using (var c = new ConsumerBuilder<Ignore, string>(conf).Build())
+			{
+				c.Subscribe("fila_pedido");
+				var cts = new CancellationTokenSource();
+
+				try
+				{
+					while (true)
+					{
+						var message = c.Consume(cts.Token);
+						_logger.LogInformation($"Mensagem: {message.Value} recebida de {message.TopicPartitionOffset}");
+					}
+				}
+				catch (OperationCanceledException)
+				{
+					c.Close();
+				}
+			}
+
+			return Task.CompletedTask;
+		}
+
+		public Task StopAsync(CancellationToken cancellationToken)
+		{
+			return Task.CompletedTask;
+		}
+	}
+}
---
+++ Kafka.Producer.API/Controllers/ProducerController.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using System.Net.Sockets;
+using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kafka.Producer.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProducerController : ControllerBase
+	{
+		[HttpPost]
+		[ProducesResponseType(typeof(string), 201)]
+		[ProducesResponseType(400)]
+		[ProducesResponseType(500)]
+		public IActionResult Post([FromQuery] string msg)
+		{
+			return Created("", SendMessageByKafka(msg));
+		}
+
+		private string SendMessageByKafka(string message)
+		{
+			/*
+			 * Rodar o comando abaixo em um terminal docker para saner o ip do container
+			 * docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' kafka
+			 */
+
+			var config = new ProducerConfig { BootstrapServers = "172.18.0.5:9092" };
+
+			using (var producer = new ProducerBuilder<Null, string>(config).Build())
+			{
+				try
+				{
+					var sendResult = producer.ProduceAsync("fila_pedido", new Message<Null, string> { Value = message })
+											 .GetAwaiter()
+											 .GetResult();
+
+					return $"Mensagem '{sendResult.Value}' de '{sendResult.TopicPartitionOffset}'";
+				}
+				catch (ProduceException<Null, string> e)
+				{
+					Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+				}
+			}
+
+			return string.Empty;
+		}
+	}
+}
+
